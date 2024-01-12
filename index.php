14a6b4f61f7dc90924172a6ce117a66cf8bf4748
Esteve Grassot <egrassot@inspalamos.cat>
2024-01-12 15:41:49 +0100

    Organitzant html

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <title>Cartoon Web!</title>
         <?php
-        require 'menu.html';
+        require 'html/menu.html';
         ?>
     </head>
     <body>
